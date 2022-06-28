99b5a2947f4aa51b81ba78cf703554eed90587f0
ImTomQ <tomcon981@gmail.com>
2022-06-28 14:24:21 +0700

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
-import Button from "@mui/material/Button";
 
 export default function App() {
   return (
     <div className="">
       <h1>Authentication</h1>
       <h3>Protected Page</h3>
-      <button >Logout</button>
+      <button>Logout</button>
     </div>
   );
 }
